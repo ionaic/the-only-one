b86043936f43eef65ccd01b537a22ab37fc27116
Javier Camino <github@jcamino.net>
2012-09-13 01:05:42 -0400

    added music playback

--- src/audio.py
+++ src/audio.py
@@ -4,6 +4,7 @@ config = ConfigParser.ConfigParser()
 config.read("../assets/audio/audioconfig.ini")
 
 mySounds={}
+mySongs={}
 pygame.init()
 
 
@@ -25,6 +26,14 @@ for sound in config.items('sounds'):
     fullname = os.path.join('../assets', sound[1])
     try:
         mySounds[sound[0]] = load_sound(fullname)
+    except pygame.error, message:
+        print 'Cannot load sound:', fullname
+        raise SystemExit, message
+
+for song in config.items('music'):
+    fullname = os.path.join('../assets', song[1])
+    try:
+        mySongs[song[0]] = load_sound(fullname)
     except pygame.error, message:
         print 'Cannot load sound:', fullname
         raise SystemExit, message
--- src/audioExample.py
+++ src/audioExample.py
@@ -1,6 +1,8 @@
+#edit the audioconfig in assets/audio to add more sounds.
 import audio
 
-audio.mySounds["trex"].play()
+audio.mySounds["trex"].play(-1)
+audio.mySongs["chase"].play()
 
 while(True):
     continue
