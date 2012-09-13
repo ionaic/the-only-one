094ac1f7a509c9bc9bb0a11414285c96ab15a7d3
Javier Camino <github@jcamino.net>
2012-09-13 01:11:54 -0400

    better comments

--- src/audioExample.py
+++ src/audioExample.py
@@ -1,7 +1,7 @@
-#edit the audioconfig in assets/audio to add more sounds.
+#also look at the audioconfig in assets/audio to add more sounds.
 import audio
 
-audio.mySounds["trex"].play(-1)
+audio.mySounds["trex"].play(-1) #loop
 audio.mySongs["chase"].play()
 
 while(True):
