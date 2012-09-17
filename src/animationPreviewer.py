f7d950a9fcd8857092682ad1fa0f9dab9d0972e9
Kevin Hendricks <kridily@optonline.net>
2012-09-16 21:19:53 -0400

    rocket animation

--- src/animationPreviewer.py
+++ src/animationPreviewer.py
@@ -39,6 +39,7 @@ characters.append(createCharacter((0,0),'../assets/tigger','object.ini','stopped
 characters.append(createCharacter((160,0),'../assets/tigger','object.ini','move'))
 characters.append(createCharacter((320,0),'../assets/static','object.ini','stuffing'))
 characters.append(createCharacter((0,160),'../assets/tigger','object.ini','launch'))
+characters.append(createCharacter((160,160),'../assets/tigger','object.ini','rocket'))
 
 #------------------------------------------------------------------------------
 
