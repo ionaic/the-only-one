6bece67166e9877c341e40a612ef3525f951ea1f
Javier Camino <github@jcamino.net>
2012-09-17 21:45:45 -0400

    step sound -still gotta add it into interactions once that's working-

--- src/audioExample.py
+++ src/audioExample.py
@@ -1,8 +1,10 @@
 #also look at the audioconfig in assets/audio to add more sounds.
 import audio
 
-audio.mySounds["trex"].play(-1) #loop
-audio.mySongs["chase"].play()
+t=audio.mySounds["trex"].play(-1) #loop
+t.stop()
+audio.mySounds["trex"].play(0) #loop
+#audio.mySongs["chase"].play()
 
 while(True):
     continue
