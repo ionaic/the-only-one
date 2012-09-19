98fb490e71d56dadb093271acca3825da4baf744
jcamino <git@jcamino.net>
2012-09-19 01:21:36 -0400

    eeyore is here!

--- src/terrain.py
+++ src/terrain.py
@@ -192,6 +192,8 @@ class CSVMap():
             CSVMap.objectData['tree'].setTag('tree')
             CSVMap.objectData['hpig'] = animatedobject.createAnimatedObject('../piglet','pig.ini')
             CSVMap.objectData['hpig'].setTag('pig')
+            CSVMap.objectData['eeyore'] = animatedobject.createAnimatedObject('../eeyore','object.ini')
+            CSVMap.objectData['eeyore'].setTag('eeyore')
 
         for dyn in dynamic:
             if dyn=='none': continue
@@ -208,6 +210,12 @@ class CSVMap():
                         self.objects.append(tmp)
                     elif char[1] == 'hpig':
                         tmp = character.Character(CSVMap.objectData['hpig'], game.Game.universal, 10, 10)
+                        tmp.setAnimation('stopped')
+                        tmp.setDirection(0)
+                        tmp.setPos(x,y)
+                        self.objects.append(tmp)
+                    elif char[1] == 'eeyore':
+                        tmp = character.Character(CSVMap.objectData['eeyore'], game.Game.universal, 10, 10)
                         tmp.setAnimation('stopped')
                         tmp.setDirection(0)
                         tmp.setPos(x,y)
